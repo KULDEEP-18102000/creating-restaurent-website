244e624d6e0c64909e2d8f13fa3bac1cd6513349
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-04-25 22:48:11 +0530

    Header added to website

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,26 @@
+import {Fragment} from "react";
+import Cart from '../../assets/cart.png'
+import Meal from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+
+
+function Header(){
+
+    return(
+        <Fragment>
+            <header className={classes.header}>
+                <h1>React Meals</h1>
+                <button>
+                    <span><img style={{width:"12px", height:"12px",marginRight:"2px"}} src={Cart} alt="cart-image"/></span>
+                    <span>Your cart</span>
+                    <span>3</span>
+                </button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={Meal} alt="Meal-image"/>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header
